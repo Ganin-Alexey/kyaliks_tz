088c8a4047a832d744ddd072611eb3c8d1bb80be
Ganin <anonim.minona.80@mail.ru>
2021-07-09 00:27:55 +0700

    TZ

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+import os
 from django.conf import settings
 from django.views import View
 from django.http import HttpResponse
 import requests
 import json
+import tempfile
 
 
 class AuthAPI(View):
@@ -14,14 +16,28 @@ class AuthAPI(View):
             "jsonrpc": "2.0",
             "id": 0,
         }
-        # Validate data
+        # Создание временных файлов и запись в них сертификатов
+        cert = tempfile.TemporaryFile(delete=False, dir=settings.BASE_DIR, mode="w+", encoding='utf-8', suffix='.crt')
+        cert.write(settings.CERTIFICATE)
+        key = tempfile.TemporaryFile(delete=False, dir=settings.BASE_DIR, mode="w+", encoding='utf-8', suffix='.key')
+        key.write(settings.CERTIFICATE_KEY)
+        cert.read()
+        key.read()
+        # Подключение к endpoint и обработка ошибок
         try:
-            response = requests.post('https://slb.medv.ru/api/v2/', cert=(settings.CERTIFICATE, settings.CERTIFICATE_KEY),
-                                    data=json.dumps(payload), headers=headers)
-            if response.json()['error']['message'] == 'Method not found':
+            response = requests.post('https://slb.medv.ru/api/v2/', cert=(cert.name, key.name),
+                                data=json.dumps(payload), headers=headers)
+            if 'error' in response and response.json()['error']['message'] == 'Method not found':
                 response = 'Такого метода не существует!'
+                # Для вывода ошибки в консоль
                 print(response)
         except requests.ConnectionError as Error:
             response = 'Ошибка подключения к серверу: ' + str(Error)
+            # Для вывода ошибки в консоль
             print(response)
+        # Закрываем и удаляем все временные файлы
+        key.close()
+        cert.close()
+        os.unlink(cert.name)
+        os.unlink(key.name)
         return HttpResponse(response)
